bd0e0393e3c06a593ef38c65479ec0448efb5e7e
Marcela Torres <itsmarcy@gmail.com>
2019-06-18 11:04:49 -0500

    Remove adding to path in topmed copy node script

--- topmed_automation.sh
+++ topmed_automation.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-# User needs to manually check space in Aspera before running script.
 PM_PATH=$1
-# The /topmed/YR3/ portion of the path
-SUB_ROOT=/groups/submissions/metadata/v1/topmed/
-SUB_PATH=${PM_PATH:35}
+SUB_ROOT=$2
+SUB_PATH=$3
 
-# Check if shepherd config exist, if not create 
-CONFIG_FILE=~/.config/shepherd.yaml
 
-if [ ! -f $CONFIG_FILE ]; then
-    cat << EOF > $CONFIG_FILE
-sub_root: /groups/submissions/metadata/v1/topmed/
-asp_root: /aspera/share/globusupload/submissions/
-EOF
-fi
-
-# if '/hgsc_software/submissions/bin' not in path
-# temporarily add it
-ACCEPT_BATCH_PATH=/hgsc_software/submissions/bin
-[[ ":$PATH:" != *":$ACCEPT_BATCH_PATH:"* ]] && PATH="$ACCEPT_BATCH_PATH:${PATH}"
-
+# Path to accept_batch script
+ACCEPT_BATCH_PATH=/hgsc_software/submissions/bin/accept_batch
 
 ## Run accept_batch
-accept_batch $1
+$ACCEPT_BATCH_PATH $1
 if [ $? != 0 ]; then
     echo "WARNING: accept_batch encountered errors."
     echo "ERROR: Stopping the pipeline."
     exit 1
 fi
+
+pushd .
 
 # Change to working directory
 cd $SUB_ROOT/$SUB_PATH
@@ -43,17 +31,17 @@ workbook=$(ls -t *_batch???_mplx.tsv | head -n1)
 if [[ $workbook =~ ' ' ]]; then
     echo 'ERROR: There is a space in the name of the workbook!!!'
     echo 'These steps will FAIL!'
+    exit 1
 fi
 
 # Assign batch name
 batch_name=$(echo $workbook | sed -e s/_mplx.tsv$//)
 
 # Run scripts to generate the copy script and md5_worklist
-stornext_path=/stornext/snfs1/submissions/topmed/topmed-code/topmed_multiplex_code/
-[[ ":$PATH:" != *":$stornext_path:"* ]] && PATH="$stornext_path:${PATH}"
-generate-topmed-copy-script-tsv $workbook
+stornext_path=/Users/marcelat/Projects/madeup
+$stornext_path/generate-topmed-copy-script-tsv $workbook
 echo "Generated copy script"
-generate-topmed-md5-worklist-tsv $workbook
+$stornext_path/generate-topmed-md5-worklist-tsv $workbook
 echo "Generated md5_worklist script"
 
 # Run validation prep steps
@@ -72,3 +60,7 @@ unset ECHO
 . ../${batch_name}_val
 echo "Successfully created symlinks"
 
+popd
+
+# If everything is successful, temporarily create file
+touch topmed_status_good
